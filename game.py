7ff63b43aedeebc0719caa66acc8a6a1a89c387c
Bilal Haroon <bilal.a.haroon@protonmail.com>
2025-12-11 23:26:46 -0600

    worlde v1

--- game.py
+++ game.py
@@ -2,32 +2,49 @@
 class Wordle:
     word_list:list[str] = [] # pull from somewhere else later
     num_of_guesses:int = 6
-    user_guesses_matrix:list[dict[str:int]] = [] # 0 for incorrect, 1 for correct but wrong place, 2 for correct
-    user_guess:list[str] = None
-    target_word:str = 'blower'
+    user_guesses_matrix:list[list[tuple]] = [] # 0 for incorrect, 1 for correct but wrong place, 2 for correct
+    user_guess:list[str] = []
+    target_word:str = 'black'
     current_guess_num = 0
-
+    win_condition = False
+    score = 6
     def guess(self, word):
-        if len(word) > 6:
-            print('word must be less than 6 chars')
-            return
         if word == self.target_word:
+            self.win_condition = True
             print('you win')
             return
 
-        wrd_dict = {}
+        word_list:list[tuple] = []
         for i,j in zip(word, self.target_word):
             if i == j:
-                wrd_dict[i] = 2
+                word_list.append((i, 2))
             elif i in self.target_word:
-                wrd_dict[i] = 1
+                word_list.append((i, 1))
             else:
-                wrd_dict[i] = 0
-        self.user_guesses_matrix.append(wrd_dict)
+                word_list.append((i, 0))
+        self.user_guesses_matrix.append(word_list)
+        self.score -= 1
         print(self.user_guesses_matrix)
-        
 
-wrdle = Wordle()
-wrdle.guess('crack')
-wrdle.guess('bronze')
+    def prompt_guess(self):
+        if len(self.user_guess) == 6:
+            self.win_condition = True
+            print('game over')
+            return
+        guess = input('Guess the five letter word: ')    
+        if len(guess) != 5:
+            print('you need to input a five letter word')
+            self.prompt_guess()
+        else:
+            self.user_guess.append(guess)
+            self.guess(guess) 
+
+
+
+    
+        
+if __name__ == '__main__':
+    wrdle = Wordle()
+    while wrdle.win_condition == False:
+        wrdle.prompt_guess()
 
---
+++ wordle_strategy.py
@@ -0,0 +1,21 @@
+import numpy as np
+import random
+from typing import List, Tuple
+
+
+
+POP_SIZE = 50
+NUM_GEN = 100
+
+
+class WordleStrategy:
+    def __init__(self, initial_guess_idx, w_g, w_y, w_e):
+        self.initial_guess_idx = initial_guess_idx
+        self.w_g = w_g
+        self.w_y = w_y
+        self.w_e = w_e
+        self.fitness = 0.0
+
+    def caclulate_guess(self, previous_feedback, possible_words):
+        if len(previous_feedback) == 0:
+            return self.initial_guess_idx
