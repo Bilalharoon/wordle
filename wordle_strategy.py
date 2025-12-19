df43721320476ff6269013ef98e406f7a4fe3ce8
Bilal Haroon <bilal.a.haroon@protonmail.com>
2025-12-19 12:43:00 -0600

    no longer repicks words with gray letters

--- wordle_strategy.py
+++ wordle_strategy.py
@@ -14,14 +14,15 @@ class SimpleWordleAgent():
     # this agent will try to narrow down the guesses based on the information
     def __init__(self, word_list):
         self.word_list = word_list
+        self.banned_letters = []
     def play(self, guess_word, feedback) -> str:
         possible_words = []
         green_letters = list(filter(lambda x: x[1] == 2, feedback))
         yellow_letters = list(filter(lambda x: x[1] == 1, feedback))
-        # if there are no green letters than pick randomly again
-        if len(green_letters) < 1:
-            # print('no green letters found')
-            return random.choice(self.word_list)
+        gray_letters = list(filter(lambda x: x[1] == 0, feedback))
+        gray_letters = [l[0] for l in gray_letters]
+        self.banned_letters += gray_letters
+        # print(gray_letters) 
 
         for word in self.word_list:
             p_feedback = Wordle.get_feedback(word, guess_word)
@@ -30,12 +31,15 @@ class SimpleWordleAgent():
             # for x,p in zip(feedback, p_feedback):
                 # if x[1] == 2 and p[1] == 2:
                     # print(x, p)
-            if all(item in p_feedback for item in green_letters):
-                possible_words.append(word)
-                # print(guess_word, green_letters, p_feedback)
+            # print(word, feedback, not any(letter[0] in self.banned_letters for letter in p_feedback))
+            if not any([letter[0] in self.banned_letters for letter in p_feedback]):
+                # print(word, guess_word, feedback)
+                if all([item in p_feedback for item in green_letters]):
+                    possible_words.append(word)
 
-        # print(len(possible_words)) 
             # print(green_feedback_letters, green_letters)
+        if len(possible_words) < 1:
+            return random.choice(self.word_list)
         self.word_list = possible_words[:]
         # print(f'list length {len(self.word_list)}')
         return random.choice(self.word_list)
@@ -54,17 +58,14 @@ if __name__ == '__main__':
     with open('./wordle-list', 'r') as file:
         possible_word_list = json.loads(file.read())
     game_won_count = 0
-    for i in range(100):
+    for i in range(1000):
         guesses = []
         simpleAgent = SimpleWordleAgent(possible_word_list)
-        # print('\n')
-        # print('\n')
-        # print(f'game {i}')
+        print('\n')
+        print('\n')
+        print(f'game {i}')
         word = random.choice(possible_word_list)
         wrdle = Wordle(possible_word_list)
-        print(f'target word: {wrdle.target_word}')
-        # print()
-        # print(f'initial word guess: {word}')
         initial_guess = wrdle.guess(word)
         while not wrdle.win_condition and wrdle.score > 0:
             feedback = wrdle.guess(word)
@@ -76,7 +77,10 @@ if __name__ == '__main__':
             print(f'won game {i} with a score of {wrdle.score}')
             print(f'target: {wrdle.target_word}\tguesses:{guesses}')
             game_won_count += 1
-    print(f'Games won out of 1000: {game_won_count}')
+        else:
+            print('game lost')
+            print(f'guesses: {guesses}')
+    print(f'Games won out of 100: {game_won_count}')
     
 
 
